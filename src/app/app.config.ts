e1f3069cbef62c9bea8810c8f0b6ca5a6da319a1
nagma <bano.nagma.1@gmail.com>
2025-06-13 16:58:45 +0530

    register the store and the effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { appEffects, appStore } from '../store/store';
+import { provideEffects } from '@ngrx/effects';
+import { ToDoService } from '../store/service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(appStore), provideEffects(appEffects), ToDoService]
 };
