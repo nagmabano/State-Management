1b32293a8ecaf3bf52116617c98b00e41839310d
nagma <bano.nagma.1@gmail.com>
2025-06-13 20:02:23 +0530

    Using store in todo component

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from '../../store/todo.model';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/store';
+import { todoSelector } from '../../store/selectors';
+import * as TodoActions from '../../store/actions';
 
 @Component({
   selector: 'app-todo',
@@ -7,5 +13,26 @@ import { Component } from '@angular/core';
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
+  todos$: Observable<Todo[]> | undefined;
+  isLoading$: Observable<boolean> | undefined;
+
+  constructor(private store: Store<AppState>) {
+    this.todos$ = this.store.select(todoSelector);
+    this.isLoading$ = this.store.select(state => state.todo.loading);
+    this.loadTodos();
+  }
+
+  loadTodos() {
+    this.store.dispatch(TodoActions.loadTodos());
+  }
+
+  addTodo(index: number) {
+    const todo: Todo = {id: index, description: "New Todo", completed: false};
+    this.store.dispatch(TodoActions.addTodo({ todo }));
+  }
+
+  complete(todo: Todo) {
+    this.store.dispatch(TodoActions.updateTodo({todo: {...todo, completed: true}}));
+  }
 
 }
